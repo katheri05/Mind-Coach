c39a34ae0171a9f8a67d451adb54d7877185622d
Katherine <katherineliu2005@gmail.com>
2021-07-20 15:20:42 -0600

    Initial Commit

---
+++ Mind Coach/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mind Coach
+//
+//  Created by Katherine Liu on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
